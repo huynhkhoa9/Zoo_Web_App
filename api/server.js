4d4dae58005c4943c4d4b68fc9137d4930ed6fbf
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 04:49:11 -0500

    connect to db

--- api/server.js
+++ api/server.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const body_parser = require("body-parser");
 const cors = require("cors");
 const authroutes = require("./routes/auth.routes")
 const mysql = require('mysql')
-
 const app = express();
 
 app.use(cors());
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(body_parser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(body_parser.urlencoded({ extended: true }));
 app.use("/", authroutes)
 
 // set port, listen for requests
@@ -21,20 +20,10 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+const db = mysql.createPool({
+  host: "zoo-database.chdju4zwao3b.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "UHCougarsCS2021$",
+  database: "ebdb"
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();
