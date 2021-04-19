365e5195c41adb3032a020a2a01eb4d144201b86
EAJ2 <78275071+EAJ2@users.noreply.github.com>
2021-04-19 16:29:31 -0500

    employee

--- src/Containers/Employees.js
+++ src/Containers/Employees.js
@@ -1,4 +1,13 @@
-function Employees()
-{
-    //comment
+import React, {useState} from "react"
+import {useHistory} from "react-router-dom"
+import EmployeeNavBar from "./EmployeeNavBar"
+import "./Employee.css"
+import axios from "axios"
+import MainPageNavBar from "./EmployeeNavBar"
+
+
+export default function Employee(){
+    var history = useHistory();
+    
+    
 }
