fef12194b38f84f3fc10cdc0f34365309a31ca07
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 14:24:24 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -18,16 +18,6 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use("/", authroutes)
 
-const db = mysql.createPool({
-  host: "zoo-database.chdju4zwao3b.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "UHCougarsCS2021$",
-  database: "ebdb"
-});
-
-app.post('http://zoowebapp-env.eba-p2mxpa2c.us-east-2.elasticbeanstalk.com:8080/api/auth/signup' , (req, res) => {
-  console.log("User Registration");
-});
 
 // set port, listen for requests
 const PORT = process.env.port || 8080;
