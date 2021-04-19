4db6acf543512a8976ae9ae4265fb931f9f277af
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 16:59:30 -0500

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
     host: "zoo-database.chdju4zwao3b.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "UHCougarsCS2021$",
-    database: "zoo"
+    database: "ebdb"
   });
 
 exports.signup = (req, res) => {
