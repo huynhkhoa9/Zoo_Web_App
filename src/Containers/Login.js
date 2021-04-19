33119b24da83a0634fc50b7609c11f3f7a91e3ad
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 04:18:32 -0500

    Update Login.js

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -7,7 +7,7 @@ import "./Login.css"
 
 function login(Username, Password){
     return axios
-        .post("/login", {
+        .post("http://zoowebapp-env.eba-p2mxpa2c.us-east-2.elasticbeanstalk.com/Login", {
             Username,
             Password
         })
