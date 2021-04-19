06e1afb905558ef723ddd3e85b9858afaffb8f3d
KennethEaso <81941725+KennethEaso@users.noreply.github.com>
2021-04-19 13:02:39 -0500

    animal card

--- src/Containers/AnimalPage.js
+++ src/Containers/AnimalPage.js
@@ -2,13 +2,27 @@ import MainPageNavBar from "./MainPageNavBar"
 import React, {useState} from "react"
 import {useHistory} from "react-router-dom"
 import axios from "axios"
+import "./AnimalPage.css"
+
+
+var animalName = "Elephant"
+var animalDescription = "Big boy"
 
 export default function AnimalPage(){
     return(
+    <div class = "AnimalPage">
+
         <div className="AnimalsPage">
             <MainPageNavBar />
             <h1>Animals in our Zoo</h1>
         </div>
+
+        <div className = "AnimalCard">
+            <h1>animalName</h1>
+            <p>animalDescription</p>
+        </div>
+
+    </div>
     )
 
 
