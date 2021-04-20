7b5f31ecc5c3fea158e6fa4bbfe0129c7827ae0e
huynhkhoa9 <huynhkhoa9@gmail.com>
2021-04-19 21:04:08 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -19,12 +19,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", authroutes)
 
-app.get("/", () =>{
- console.log("Welcome to the Homepage1");
-})
-
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+app.get("/", () =>{
+  console.log("Welcome to the Homepage1");
+ })
